d127689eda72994bf97ef47ea8e2b252520b236c
nishadinfo09 <mdnishadali03@gmail.com>
2026-09-05 05:57:13 +0600

    code rabbit suggestion fixes

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -54,7 +54,7 @@ export default function App() {
               >
               </FlatList>
             </View>
-            <ListHeading title="All Subscription" />
+            <ListHeading title="All Subscriptions" />
           </>
         )}
 
