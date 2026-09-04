951363e982289110d1ddf845ce89a0348fa40392
nishadinfo09 <mdnishadali03@gmail.com>
2026-09-04 21:30:40 +0600

    implement font

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -8,32 +8,21 @@ const SafeAreaView = styled(RNSafeAreaView)
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Hello World
-      </Text>
+      <Text className="text-7xl font-sans-extrabold">Home</Text>
+      <Text className="text-7xl font-bold">Home</Text>
 
-      <Link href={'/onbording'} className="mt-4 rounded bg-primary text-lg text-white p-4">
+      <Link href={'/onbording'} className="mt-4 font-sans-bold rounded bg-primary text-lg text-white p-4">
       Go to onbording
       </Link>
 
-      <Link href={'/(auth)/sign-in'} className="mt-4 rounded bg-primary text-lg text-white p-4">
+      <Link href={'/(auth)/sign-in'} className="mt-4 font-sans-bold rounded bg-primary text-lg text-white p-4">
       Go to Sign In
       </Link>
 
-      <Link href={'/(auth)/sign-up'} className="mt-4 rounded bg-primary text-lg text-white p-4">
+      <Link href={'/(auth)/sign-up'} className="mt-4 font-sans-bold rounded bg-primary text-lg text-white p-4">
       Go to Sign Up
       </Link>
 
-      <Link href='/subscriptions/spotify' className="mt-4 rounded bg-primary text-lg text-white p-4">
-      Go to Spotify
-      </Link>
-
-      <Link href={{
-        pathname: '/subscriptions/[id]',
-        params: {id: 'claude'}
-      }} className="mt-4 rounded bg-primary text-lg text-white p-4">
-      Go to claude
-      </Link>
     </SafeAreaView>
   );
 }
