ed9b824b6d40cf7cdd2f73f41bee5a3fd5d9a39d
nishadinfo09 <mdnishadali03@gmail.com>
2026-09-04 22:45:06 +0600

    add avatar, add icon and balance card

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,28 +1,31 @@
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import "@/global.css";
-import { Link } from "expo-router";
 import {styled} from 'nativewind'
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import images from "@/constaints/images";
+import { HOME_BALANCE, HOME_USER } from "@/constaints/data";
+import { icons } from "@/constaints/icon";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from 'dayjs'
 const SafeAreaView = styled(RNSafeAreaView)
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-7xl font-sans-extrabold">Home</Text>
-      <Text className="text-7xl font-bold">Home</Text>
-
-      <Link href={'/onbording'} className="mt-4 font-sans-bold rounded bg-primary text-lg text-white p-4">
-      Go to onbording
-      </Link>
-
-      <Link href={'/(auth)/sign-in'} className="mt-4 font-sans-bold rounded bg-primary text-lg text-white p-4">
-      Go to Sign In
-      </Link>
-
-      <Link href={'/(auth)/sign-up'} className="mt-4 font-sans-bold rounded bg-primary text-lg text-white p-4">
-      Go to Sign Up
-      </Link>
-
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar"/>
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
+        <Image source={icons.add} className="home-add-icon"/>
+      </View>
+      <View className="home-balance-card">
+        <Text className="home-balance-label">Balance</Text>
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">{formatCurrency(HOME_BALANCE.amount)}</Text>
+          <Text className="home-balance-date">{dayjs(HOME_BALANCE.nextRenewalDate).format('MM/DD')}</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
